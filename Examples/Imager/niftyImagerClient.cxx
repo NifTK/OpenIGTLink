68358738302dfc90576066c92a76c30e3188fcdf
Steve Thompson <s.thompson@ucl.ac.uk>
2012-10-25 11:34:12 +0100

    feature #1763 set up niftyImagerClient to send tracking data as well as matrices

--- Examples/Imager/niftyImagerClient.cxx
+++ Examples/Imager/niftyImagerClient.cxx
@@ -21,36 +21,115 @@
 
 #include "igtlOSUtil.h"
 #include "igtlImageMessage.h"
+#include "igtlTrackingDataMessage.h"
 #include "igtlStringMessage.h"
 #include "igtlClientSocket.h"
 
 int GetTestImage(igtl::ImageMessage::Pointer& msg, const char* dir, int i);
-void GetRandomTestMatrix(igtl::Matrix4x4& matrix);
+int GetTestMatrix(igtl::TrackingDataElement::Pointer& msg, const char* dir, int i);
+void SetMotorPosition (igtl::Matrix4x4& matrix, float pos = 47.25);
 // Modified imager client, designed as a command line mimic of 
 // an ultrasonix imager client
-int main(int argc, char* argv[])
+enum {
+	IMAGE_MESSAGE = 0 ,
+	TRACKING_MESSAGE = 1
+};
+void usage ( char * argv[]) 
 {
-  //------------------------------------------------------------
-  // Parse Arguments
-
-  if (argc != 5) // check number of arguments
-    {
     // If not correct, print usage
-    std::cerr << "Usage: " << argv[0] << " <hostname> <port> <fps> <imgdir>"    << std::endl;
-    std::cerr << "    <hostname> : IP or host name, default to localhost"                    << std::endl;
-    std::cerr << "    <port>     : Port # (3200 in Slicer default)"   << std::endl;
-    std::cerr << "    <fps>      : Frequency (fps) to send coordinate" << std::endl;
-    std::cerr << "    <imgdir>   : file directory, where \"igtlTestImage[1-5].raw\" are placed." << std::endl;
-    std::cerr << "                 (usually, in the Examples/Imager/img directory.)" << std::endl;
+    std::cerr << "Usage: " << argv[0]  << std::endl;
+    std::cerr << "    --hostname <hostname>  : IP or host name, default to localhost"                    << std::endl;
+    std::cerr << "    --port <port>          : Port # (3200 in Slicer default)"   << std::endl;
+    std::cerr << "    --fps  <fps>           : Frequency (fps) to send coordinate [1]" << std::endl;
+    std::cerr << "    --filedir <filedir>    : file directory, where \"igtlTestImage[1-5].raw\" are placed." << std::endl;
+    std::cerr << "                             (usually, in the Examples/Imager/img directory.)" << std::endl;
+    std::cerr << "    --repeats <repeats>    : Number of messages to send [20]" << std::endl;
+    std::cerr << "    --messagetype <type>   : Type of message to send (image or trackingdata)" << std::endl;
     exit(0);
-    }
+}
+int main(int argc, char** argv)
+{
 
-  char*  hostname = argv[1];
-  int    port     = atoi(argv[2]);
-  double fps      = atof(argv[3]);
+
+  char hostname[255];
+  int    port     = 3200;
+  double fps      = 1;
+  char  filedir[512];
+	int    messagetype = IMAGE_MESSAGE;
+	int    repeats = 20;
+	strcpy ( hostname , "localhost"); 
+	strcpy (filedir,  "../Examples/Imager/img/");																
+	bool filedirSet=false;
+	while ( argc > 1 ) 
+	{
+		bool ok = false; 
+	  if ((ok ==false) && (strcmp(argv[1], "--hostname" ) == 0 ) )
+		{
+			argc--;
+			argv++;
+			strcpy(hostname,argv[1]);
+			argc--;
+			argv++;
+			ok=true;
+		}
+		if ((ok ==false) && (strcmp(argv[1], "--port" ) == 0 ) )
+		{
+			argc--;
+			argv++;
+			port = atoi (argv[1]);
+			argc--;
+			argv++;
+			ok=true;
+		}
+		if ((ok ==false) && (strcmp(argv[1], "--fps" ) == 0 ) )
+		{
+			argc--;
+			argv++;
+			fps = strtod (argv[1],NULL);
+			argc--;
+			argv++;
+			ok=true;
+		}
+   if ((ok ==false) && (strcmp(argv[1], "--filedir" ) == 0 ) )
+		{
+			argc--;
+			argv++;
+			strcpy(filedir,argv[1]);
+			filedirSet = true;
+			argc--;
+			argv++;
+			ok=true;
+		}
+		if ((ok ==false) && (strcmp(argv[1], "--repeats" ) == 0 ) )
+		{
+			argc--;
+			argv++;
+			repeats = atoi (argv[1]);
+			argc--;
+			argv++;
+			ok=true;
+		}
+	  if ((ok ==false) && (strcmp(argv[1], "--messagetype" ) == 0 ) )
+		{
+			argc--;
+			argv++;
+			if ( strcmp ( argv[1],"image" ) == 0 )
+				messagetype=IMAGE_MESSAGE;
+			if ( strcmp ( argv[1],"trackingdata" ) == 0 ) 
+				messagetype=TRACKING_MESSAGE;
+			argc--;
+			argv++;
+			ok=true;
+		}
+		if ( ok == false ) 
+			usage (argv) ;
+	}
+
+	if ( ! filedirSet && ( messagetype == TRACKING_MESSAGE ) )
+		strcpy (filedir,  "../Examples/Imager/matrices/");																
+
+		
   int    interval = (int) (1000.0 / fps);
-  char*  filedir  = argv[4];
-
   //------------------------------------------------------------
   // Establish Connection
   igtl::ClientSocket::Pointer socket;
@@ -65,47 +144,78 @@ int main(int argc, char* argv[])
 
 	// Create a string message to initiate the connection
 	igtl::StringMessage::Pointer infoMsg = igtl::StringMessage::New();
-	infoMsg->SetString("<ClientDescriptor><Device CommunicationType=\"Porta Interface\" DeviceName=\"Ultrasonix\" PortName=\"\" DeviceType=\"Imager\"/><Client ClientIP=\"UNKNOWN\" ClientPort=\"3200\"/></ClientDescriptor>");
+	switch ( messagetype ) 
+	{
+		case IMAGE_MESSAGE:
+			infoMsg->SetString("<ClientDescriptor><Device CommunicationType=\"Porta Dummy\" DeviceName=\"Ultrasonix\" PortName=\"\" DeviceType=\"Imager\"/><Client ClientIP=\"UNKNOWN\" ClientPort=\"3200\"/></ClientDescriptor>");
+			break;
+		case TRACKING_MESSAGE:
+			infoMsg->SetString("<ClientDescriptor><Device CommunicationType=\"Serial\" DeviceName=\"NDI Polaris Vicra\" PortName=\"\" DeviceType=\"Tracker\"/><Client ClientIP=\"UNKNOWN\" ClientPort=\"3200\"/></ClientDescriptor>");
+			break;
+		default:
+			infoMsg->SetString("<ClientDescriptor><Device CommunicationType=\"UNKNOWN\" DeviceName=\"UNKNOWN\" PortName=\"\" DeviceType=\"UNKNOWN\"/><Client ClientIP=\"UNKNOWN\" ClientPort=\"3200\"/></ClientDescriptor>");
+	}
 
   infoMsg->Pack();
   socket->Send(infoMsg->GetPackPointer(), infoMsg->GetPackSize());
   //------------------------------------------------------------
   // loop
-  for (int i = 0; i < 2; i ++)
+  for (int i = 0; i < repeats; i ++)
     {
 
-    //------------------------------------------------------------
-    // size parameters
-    int   size[]     = {256, 256, 1};       // image dimension
-    float spacing[]  = {1.0, 1.0, 5.0};     // spacing (mm/pixel)
-    int   svsize[]   = {256, 256, 1};       // sub-volume size
-    int   svoffset[] = {0, 0, 0};           // sub-volume offset
-    int   scalarType = igtl::ImageMessage::TYPE_UINT8;// scalar type
+		switch ( messagetype ) 
+		{
+		case IMAGE_MESSAGE:
+			{	//------------------------------------------------------------
+			// size parameters
+			int   size[]     = {256, 256, 1};       // image dimension
+			float spacing[]  = {1.0, 1.0, 5.0};     // spacing (mm/pixel)
+			int   svsize[]   = {256, 256, 1};       // sub-volume size
+			int   svoffset[] = {0, 0, 0};           // sub-volume offset
+			int   scalarType = igtl::ImageMessage::TYPE_UINT8;// scalar type
 
-    //------------------------------------------------------------
-    // Create a new IMAGE type message
-    igtl::ImageMessage::Pointer imgMsg = igtl::ImageMessage::New();
-    imgMsg->SetDimensions(size);
-    imgMsg->SetSpacing(spacing);
-    imgMsg->SetScalarType(scalarType);
-    imgMsg->SetDeviceName("ImagerClient");
-    imgMsg->SetSubVolume(svsize, svoffset);
-    imgMsg->AllocateScalars();
+			//------------------------------------------------------------
+			// Create a new IMAGE type message
+			igtl::ImageMessage::Pointer imgMsg = igtl::ImageMessage::New();
+			imgMsg->SetDimensions(size);
+			imgMsg->SetSpacing(spacing);
+			imgMsg->SetScalarType(scalarType);
+			imgMsg->SetDeviceName("ImagerClient");
+			imgMsg->SetSubVolume(svsize, svoffset);
+			imgMsg->AllocateScalars();
 
-    //------------------------------------------------------------
-    // Set image data (See GetTestImage() bellow for the details)
-    GetTestImage(imgMsg, filedir, i % 5);
+			//------------------------------------------------------------
+			// Set image data (See GetTestImage() bellow for the details)
+			GetTestImage(imgMsg, filedir, i % 5);
 
-    //------------------------------------------------------------
-    // Get random orientation matrix and set it.
-    igtl::Matrix4x4 matrix;
-    GetRandomTestMatrix(matrix);
-    imgMsg->SetMatrix(matrix);
+			//------------------------------------------------------------
+			// Get random orientation matrix and set it.
+			igtl::Matrix4x4 matrix;
+			SetMotorPosition(matrix);
+			imgMsg->SetMatrix(matrix);
 
-    //------------------------------------------------------------
-    // Pack (serialize) and send
-    imgMsg->Pack();
-    socket->Send(imgMsg->GetPackPointer(), imgMsg->GetPackSize());
+			//------------------------------------------------------------
+			// Pack (serialize) and send
+			imgMsg->Pack();
+			socket->Send(imgMsg->GetPackPointer(), imgMsg->GetPackSize());
+			break;
+			}
+		case TRACKING_MESSAGE:
+			{
+			igtl::TrackingDataMessage::Pointer trkMsg = igtl::TrackingDataMessage::New();
+			igtl::TrackingDataElement::Pointer trkEl = igtl::TrackingDataElement::New();
+			
+			GetTestMatrix ( trkEl, filedir, i % 1000 );
+			trkEl->SetName("8700338.rom");
+			trkEl->SetType(3);
+			trkMsg->AddTrackingDataElement(trkEl);
+
+			trkMsg->Pack();
+			socket->Send(trkMsg->GetPackPointer(),trkMsg->GetPackSize());
+			break;
+			}
+		}
+
 
     igtl::Sleep(interval); // wait
 
@@ -142,7 +252,7 @@ int GetTestImage(igtl::ImageMessage::Pointer& msg, const char* dir, int i)
   FILE *fp = fopen(filename, "rb");
   if (fp == NULL)
     {
-    std::cerr << "File opeining error: " << filename << std::endl;
+    std::cerr << "File opening error: " << filename << std::endl;
     return 0;
     }
   int fsize = msg->GetImageSize();
@@ -154,42 +264,55 @@ int GetTestImage(igtl::ImageMessage::Pointer& msg, const char* dir, int i)
 
   return 1;
 }
+int GetTestMatrix(igtl::TrackingDataElement::Pointer& msg, const char* dir, int i)
+{
+
+  //------------------------------------------------------------
+  // Check if image index is in the range
+  if (i < 0 || i >= 999) 
+    {
+    std::cerr << "Matrix index is invalid." << std::endl;
+    return 0;
+    }
+
+  //------------------------------------------------------------
+  // Generate path to the raw image file
+  char filename[128];
+  sprintf(filename, "%s/igtlTestMatrix%04d.matrix", dir, i+1);
+  std::cerr << "Reading " << filename << "...";
+
+  //------------------------------------------------------------
+  // Load raw data from the file
+  FILE *fp = fopen(filename, "r");
+  if (fp == NULL)
+    {
+    std::cerr << "File opening error: " << filename << std::endl;
+    return 0;
+    }
+	igtl::Matrix4x4 matrix;
+		for ( int row = 0 ; row < 4 ; row ++)
+	{
+		for ( int col = 0 ; col < 4 ; col ++)
+			fscanf(fp , "%f", &matrix[row][col]);
+	}
+
+  fclose(fp);
+	msg->SetMatrix(matrix);	
+
+  std::cerr << "done." << std::endl;
+
+  return 1;
+}
+
 
 //------------------------------------------------------------
 // Function to generate random matrix.
-void GetRandomTestMatrix(igtl::Matrix4x4& matrix)
+void SetMotorPosition(igtl::Matrix4x4& matrix, float pos)
 {
-  //float position[3];
-  //float orientation[4];
-
-  /*
-  // random position
-  static float phi = 0.0;
-  position[0] = 50.0 * cos(phi);
-  position[1] = 50.0 * sin(phi);
-  position[2] = 0;
-  phi = phi + 0.2;
-
-  // random orientation
-  static float theta = 0.0;
-  orientation[0]=0.0;
-  orientation[1]=0.6666666666*cos(theta);
-  orientation[2]=0.577350269189626;
-  orientation[3]=0.6666666666*sin(theta);
-  theta = theta + 0.1;
-
-  igtl::Matrix4x4 matrix;
-  igtl::QuaternionToMatrix(orientation, matrix);
-
-  matrix[0][3] = position[0];
-  matrix[1][3] = position[1];
-  matrix[2][3] = position[2];
-  */
-
   matrix[0][0] = 1.0;  matrix[1][0] = 0.0;  matrix[2][0] = 0.0; matrix[3][0] = 0.0;
-  matrix[0][1] = 0.0;  matrix[1][1] = -1.0;  matrix[2][1] = 0.0; matrix[3][1] = 0.0;
+  matrix[0][1] = 0.0;  matrix[1][1] = 1.0;  matrix[2][1] = 0.0; matrix[3][1] = 0.0;
   matrix[0][2] = 0.0;  matrix[1][2] = 0.0;  matrix[2][2] = 1.0; matrix[3][2] = 0.0;
-  matrix[0][3] = 0.0;  matrix[1][3] = 0.0;  matrix[2][3] = 0.0; matrix[3][3] = 1.0;
+  matrix[0][3] = pos;  matrix[1][3] = 0.0;  matrix[2][3] = 0.0; matrix[3][3] = 1.0;
   
   igtl::PrintMatrix(matrix);
 }
